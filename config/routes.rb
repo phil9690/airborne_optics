729ae746ac00d059b761b463fef9c7d39674ed29
Phil <p.clauzel90@gmail.com>
2015-07-14 11:33:03 +0100

    Add new static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
+  get 'aerial-photography' => 'static_pages#photography'
+  get 'aerial-videography' => 'static_pages#videography'
+  get 'video-editing' => 'static_pages#editing'
 
 end
